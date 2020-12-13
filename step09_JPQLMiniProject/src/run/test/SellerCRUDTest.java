5305f6eb74ddc730d539ef6e225886044d5622c8
JANGMOONHEE <reolove7029@gmail.com>
2020-12-13 15:22:50 +0900

    updateCRUDTest

--- step09_JPQLMiniProject/src/run/test/SellerCRUDTest.java
+++ step09_JPQLMiniProject/src/run/test/SellerCRUDTest.java
@@ -11,20 +11,25 @@ public class SellerCRUDTest {
 	public void updateSeller() {
 		EntityManager em = PublicCommon.getEntityManager();
 		EntityTransaction tx = em.getTransaction();
-		tx.begin();
-		Seller seller = em.find(Seller.class, "yoons1234");
-		if(seller != null) {
-			//before update
-			System.out.println("update 전 : " + seller);
-			seller.setResell_price(410000);
-		}else {
-			System.out.println("업데이트 할 수 있는 상품이 없습니다.");
-		}
-		tx.commit();
-		//after update
-		System.out.println("update 후 : " + seller);
-		
-		
+		try {
+			tx.begin();
+			Seller seller = em.find(Seller.class, "yoons1234");
+			if(seller != null) {
+				//before update
+				System.out.println("update 전 : " + seller);
+				seller.setResell_price(410000);
+			}else {
+				System.out.println("업데이트 할 수 있는 상품이 없습니다.");
+			}
+			tx.commit();
+			//after update
+			System.out.println("update 후 : " + seller);
+		}catch (Exception e) {
+		e.printStackTrace();
+	}finally {
+		em.close();
 	}
+}
+	
 
 }
